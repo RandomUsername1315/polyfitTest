b8bac1c1f02bb9fe61172f6e9fbe6787239a2fcc
RandomUsername1315 <charles.rousset@epfl.ch>
2024-03-19 13:18:37 +0100

    asdf

--- app/src/main/java/com/github/se/polyfit/MainActivity.kt
+++ app/src/main/java/com/github/se/polyfit/MainActivity.kt
@@ -26,6 +26,9 @@ class MainActivity : ComponentActivity() {
                         LoginScreen(navigation)
                     }
 
+
+
+
                     
 
                     composable(Route.Home) {
